13416f976544e083b9cbb167657f3036ca93b703
delicha <shoheiyamamoto919@gmail.com>
2022-12-01 18:24:52 +0900

    second commit

---
+++ src/app/Http/Controllers/TasksController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use App\Http\Requests\TaskRequest;
+
+class TasksController extends Controller
+{
+    public function index() {
+        $tasks = Task::orderBy('updated_at', 'desc')->get();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function show($id) {
+        $task = Task::find($id);
+        return view('tasks.show', compact('task'));
+    }
+
+    public function add() {
+        return view('tasks.add');
+    }
+
+    public function store(TaskRequest $request) {
+        $result = Task::create([
+            'name' => $request->name,
+            'content' => $request->content,
+        ]);
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function edit($id) {
+        $task = Task::find($id);
+        return view('tasks.edit', compact('task'));
+    }
+
+    public function update(TaskRequest $request, $id) {
+        $task = Task::find($id);
+        $task->fill([
+            'name' => $request->name,
+            'content' => $request->content,
+        ])
+        ->save();
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function delete($id) {
+        $task = Task::destroy($id);
+
+        return redirect()->route('tasks.index');
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [TasksController::class, 'index'])->name('tasks.index');
+
+Route::get('/{id}', [TasksController::class, 'show'])->name('tasks.show');
+
+Route::get('/tasks/add', [TasksController::class, 'add'])->name('tasks.add');
+
+Route::post('/tasks/add', [TasksController::class, 'store'])->name('tasks.store');
+
+Route::get('/tasks/edit/{id}', [TasksController::class, 'edit'])->name('tasks.edit');
+
+Route::post('/tasks/edit/{id}', [TasksController::class, 'update'])->name('tasks.update');
+
+Route::post('/tasks/delete/{id}', [TasksController::class, 'delete'])->name('tasks.delete');
+
+// リソースコントローラー例 Route: :resource('/sample', 'SampleController', ['only' =› ['index', 'show', 'edit', 'update']]);
